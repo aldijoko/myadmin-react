6fad2f0e03779bfdc7b3ce85c8faa6d244f1269f
aldijoko <aldijokosp22>
2023-03-03 18:11:56 +0700

    update for adding icon Mode

--- src/pages/global/Topbar.jsx
+++ src/pages/global/Topbar.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
 
 const Topbar = () => {
   return <div>Topbar</div>;
